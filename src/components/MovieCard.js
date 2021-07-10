ed02eda070b4ad7e3eff66059e267d473716f0e9
Rishabh Jain <rishabhkjain1999@outlook.com>
2021-07-10 16:40:24 +0530

    Redux

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { Component } from 'react';
+import { addToFavourites, removeFromFavourites } from '../actions';
 
-class MovieCard extends React.Component {
+class MovieCard extends Component {
+  handleFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(addToFavourites(movie));
+  };
 
-  render(){
-    const{movie}=this.props;
+  handleUnFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(removeFromFavourites(movie));
+  };
+
+  render() {
+    const { movie, isFavourite } = this.props;
     return (
-        <div className="movie-card">
-          <div className="left">
-            <img src={movie.Poster} alt="movie-pic" />
+      <div className="movie-card">
+        <div className="left">
+          <img src={movie.Poster} alt="movie-pic" />
+        </div>
+        <div className="right">
+          <div className="title">
+            {movie.Title} ({movie.Year})
           </div>
-          <div className="right">
-            <div className="title">{movie.Title} ({movie.Year})</div>
-            <div className="plot">{movie.Plot}</div>
-            <div className="footer">
-              <div className="rating">{movie.imdbRating}</div>
-              <button className="favourite-btn">Favourite</button>
-            </div>
+          <div className="plot">{movie.Plot}</div>
+          <div className="footer">
+            <div className="rating">{movie.imdbRating}</div>
+            {isFavourite ? (
+              <button
+                className="unfavourite-btn"
+                onClick={this.handleUnFavouriteClick}
+              >
+                Unfavourite
+              </button>
+            ) : (
+              <button
+                className="favourite-btn"
+                onClick={this.handleFavouriteClick}
+              >
+                Favourite
+              </button>
+            )}
           </div>
         </div>
-      );
+      </div>
+    );
   }
 }
 
